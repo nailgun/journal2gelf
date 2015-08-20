33bda4c3cd8893a8cee139d0cdf898b1dd337f7f
Dmitry Bashkatov <dbashkatov@gmail.com>
2015-08-20 14:38:18 +0300

    bump version to 1.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='journal2gelf',
-    version='1.0.5',
+    version='1.0.6',
     description='Export structured log records from a systemd journal and send them to a Graylog2 server.',
     url='https://github.com/nailgun/journal2gelf',
     author='Nailgun',
